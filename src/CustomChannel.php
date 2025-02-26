5d9ccca0f3098445bf1e072199770c7a8c1784b0
Marek Hučík <marek.hucik@reservio.com>
2025-02-26 08:42:39 +0100

    Fix monolog/monolog compatibility

--- src/CustomChannel.php
+++ src/CustomChannel.php
@@ -81,9 +81,9 @@ class CustomChannel extends \Kdyby\Monolog\Logger
 	/**
 	 * {@inheritdoc}
 	 */
-	public function addRecord(int $level, string $message, array $context = []): bool
+	public function addRecord(int $level, string $message, array $context = [], ?DateTimeImmutable $datetime = null): bool
 	{
-		return $this->parentLogger->addRecord($level, $message, array_merge(['channel' => $this->name], $context));
+		return $this->parentLogger->addRecord($level, $message, array_merge(['channel' => $this->name], $context), $datetime);
 	}
 
 	/**
